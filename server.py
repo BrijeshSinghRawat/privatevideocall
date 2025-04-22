8ac21e0270c22ef5d93522be3eb5c89883224df2
BrijeshSinghRawat <123789b@gmail.com>
2025-04-22 15:21:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,28 +7,34 @@ sio = socketio.AsyncServer(async_mode='asgi')
 app.mount("/socket.io", socketio.ASGIApp(sio))
 app.mount("/", StaticFiles(directory="public", html=True), name="public")
 
-ROOM = "private-room"
-
 @sio.event
 async def connect(sid, environ):
     print(f"User connected: {sid}")
-    await sio.enter_room(sid, ROOM)
 
 @sio.event
 async def disconnect(sid):
     print(f"User disconnected: {sid}")
 
 @sio.event
+async def join_room(sid, data):
+    room = data['room']
+    sio.enter_room(sid, room)
+    print(f"User {sid} joined room {room}")
+
+@sio.event
 async def offer(sid, data):
-    await sio.emit('offer', data, room=ROOM, skip_sid=sid)
+    room = data['room']
+    await sio.emit('offer', data, room=room, skip_sid=sid)
 
 @sio.event
 async def answer(sid, data):
-    await sio.emit('answer', data, room=ROOM, skip_sid=sid)
+    room = data['room']
+    await sio.emit('answer', data, room=room, skip_sid=sid)
 
 @sio.event
 async def ice_candidate(sid, data):
-    await sio.emit('ice-candidate', data, room=ROOM, skip_sid=sid)
+    room = data['room']
+    await sio.emit('ice-candidate', data, room=room, skip_sid=sid)
 
 if __name__ == "__main__":
     import uvicorn
